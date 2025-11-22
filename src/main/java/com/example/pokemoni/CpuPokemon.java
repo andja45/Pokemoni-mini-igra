7e953b758e9bf1314746a2b277366f7e1f68c03f
andja <117669334+andja45@users.noreply.github.com>
2025-11-22 15:59:23 +0100

    Brisanje komentara
    Removed comments for CpuPokemon class implementation details.

--- src/main/java/com/example/pokemoni/CpuPokemon.java
+++ src/main/java/com/example/pokemoni/CpuPokemon.java
@@ -2,11 +2,6 @@ package com.example.pokemoni;
 
 import java.util.Random;
 
-/*  Napraviti klasu CpuPokemon koja nasleđuje klasu Pokemon. Klasa poseduje privatno statičko polje random (Random).
-    Implementirati metod public void baciMagiju(Pokemon neprijatelj) tako da sa verovatnoćom 0.5 baca prvu
-    magiju, i sa verovatnoćom 0.5 baca drugu magiju na prosleđenog neprijatelja.
-    Klasa Random poseduje metod nextDouble koji generiše pseudo-slučajni broj iz intervala [0, 1] i može Vam koristiti u
-    implementaciji nekog metoda klase. */
 public class CpuPokemon extends Pokemon{
     private Magija PoslednjaBacena;
 
