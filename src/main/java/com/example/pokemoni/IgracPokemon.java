924d94786d6a33744fa6e011f4c52aeda726cdf5
andja <117669334+andja45@users.noreply.github.com>
2025-11-22 15:59:43 +0100

    Update IgracPokemon.java
    Removed comments detailing class implementation for IgracPokemon.

--- src/main/java/com/example/pokemoni/IgracPokemon.java
+++ src/main/java/com/example/pokemoni/IgracPokemon.java
@@ -1,9 +1,5 @@
 package com.example.pokemoni;
-/*  Napraviti klasu IgracPokemon koja nasleđuje klasu Pokemon. Igrač poseduje atribut izabranaMagija (int) čije vred-
-    nosti mogu biti 1 ili 2. Implementirati metod Magija getIzabranaMagija() koji vraća prvu ili drugu magiju u
-    zavisnosti od vrednosti atributa izabranaMagija i metod void setIzabranaMagija(int i) koja postavlja izabranu
-    magiju na prvu ukoliko je i=1, na drugu ukoliko je i=2, a inače ne radi ništa. Implementirati metod public void
-    baciMagiju(Pokemon neprijatelj) tako da prosleđenom pokemonu nanosi štetu koju poseduje izabrana magija. */
+
 public class IgracPokemon extends Pokemon {
     private int izabranaMagija;
     public IgracPokemon(String ime, int nivo, Magija magija1, Magija magija2){
